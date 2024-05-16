41e66f3264e07423251f4dfbef942e4b3c66c66e
Girma <93812274+206724@users.noreply.github.com>
2024-05-16 02:07:49 -0700

    my second commit

---
+++ Script.mjs
@@ -0,0 +1,405 @@
+// The provided course information.
+const CourseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+  };
+  
+  // The provided assignment group.
+  const AssignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      },
+      {
+        id: 3,
+        name: "Code the World",
+        due_at: "3156-11-15",
+        points_possible: 500
+      }
+    ]
+  };
+  
+  // The provided learner submission data.
+  const LearnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
+    }
+  ];
+  
+  function getLearnerData(course, ag, submissions) {
+  }
+  
+  LearnerSubmissions.filter(function (item) {
+    // console.log(item);
+  });
+
+  const search = LearnerSubmissions.filter(function (item) {
+    return item.learner_id===125 ;
+  });
+  console.log(search)
+
+
+
+  
+//collect learner id number from learsummtion
+let learner_summsion = (LearnerSubmissions) => { 
+    let output = []; 
+    for (let item of LearnerSubmissions) { 
+        output.push(item.learner_id); 
+    } 
+    return output; 
+}; 
+
+let result = learner_summsion(LearnerSubmissions); 
+console.log(result);
+
+
+// this function helps to delet dupliate
+function removeDuplicates(result) {
+    return result.filter((item,
+        index) => result.indexOf(item) === index);
+}
+let result1=(removeDuplicates(result));
+console.log(result1);
+
+function passToFunction(){
+    let arrayToPass =result1;
+    unmodifiableFunction(...arrayToPass);
+}
+function unmodifiableFunction(a, b) {
+    console.log(`learner_id: ${a}
+learner_id: ${b}`)
+
+}
+passToFunction()
+
+
+
+// const learnerSubmission_learner_id=LearnerSubmissions.map(obj => 
+//     { if (obj.learner_id===125){
+//         return{...obj,assignment_id:1};
+//     }
+//     return obj;
+//   })
+// console.log(learnerSubmission_learner_id)
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  // here, we would process this data to achieve the desired result.
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
+  
+//     return result;
+//   }
+  
+//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  
+//   console.log(result);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let number;
+// let Date_string ;
+
+
+// let CourseInfo= {"id":number,"name": String };
+// let AssignmentInfo = {"id":number,"name": String,"due_at" : Date_string,"points_possible": number}
+// let AssignmentGroup={"id": number,"name": String,"course_id" : number,"group_weight" : number,"assignment" : [AssignmentInfo]}
+// let LearnerSubmissions ={"learner_id":number,assignment_id: number,"submission":{"submitted_at": Date_string,"scrore":number}}
+
+// let learner_id_ =getLearnerId(LearnerSubmissions);
+// let weighted_avg= getAverage(AssignmentGroup);
+// let assignmnet_ID=getAssID(LearnerSubmissions);
+
+// function getLearnerData(course, AssignmentGroup, submissions) {
+//   // here, we would process this data to achieve the desired result.
+  
+// getLearnerId(LearnerSubmissions)
+// {
+// let course_id =102;
+// return (course_id);   
+// }
+
+// getAverage(AssignmentGroup)
+// {
+//     let score1;
+//     let score2;
+//     let total_scrore=score1+score2;//47+150
+//     let total_points_possible=points_possible1+points_possible2;//50+150     let weighted_avg =total_scrore/total_points_possible;
+//     return weighted_avg;
+
+// }
+
+// getAssID(LearnerSubmissions)
+// {
+//  let assignment_id ;
+//  return assignment_id;
+// }
+
+  
+//  }
+
+
+
+// let course_id =100;
+
+
+// if(course_id !==102 || 103 || 104)
+    
+//     try{
+//     {  
+   
+//     throw " error - please insert valid course_id";
+//     }
+// }
+// catch(error){
+//     console.log("please insert the correct value")
+// }
+
+// try{
+// if (points_possible<0) throw " Please insert the correct number";{
+
+// throw " error - please insert valid course_id";
+// }
+// }
+// catch(error){
+// console.log("please insert the correct value")
+// }
+
+// let submitted_at;
+// let due_at;
+// let learner_total;
+// if ( submitted_at > due_at){
+
+//     learner_total = learner_total;
+// }
+//  else
+//      learner_total =learner_total - learner_total*0.15; 
+
+
+
+
+
+
+ 
+ 
+//   const result = [
+//     {
+//       id: 125,
+//       avg: 0.985, // (47 + 150) / (50 + 150)
+//       1: 0.94, // 47 / 50
+//       2: 1.0 // 150 / 150
+//     },
+//     {
+//       id: 132,
+//       avg: 0.82, // (39 + 125) / (50 + 150)
+//       1: 0.78, // 39 / 50
+//       2: 0.833 // late: (140 - 15) / 150
+//     }
+//   ];
+
+//   return result;
+// }
+
+// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const str= "hello world";
+// // for (let i =0;i<str.length;i++){
+ 
+// // console.log(str[i])
+// // }
+
+// const str = "henllo world";
+// for (const i in str){
+//     console.log(str[i])
+// }
+
+
+// const student = {
+//     name: "Monica",
+//     class: 7,
+//     age: 12,
+// //   };
+  
+// //   for (let key in student) {
+// //     console.log(`${key} *** ${student[key]}`);
+// //   }
+// let sum = 0;
+
+// for (let i = 0; i < 5; i++) {
+
+//   if (i % 2 === 0) continue;
+
+//   sum += i;
+
+// }
+
+// const names = ["John", "Abagail", "Jerome", "Cynthia"];
+// const greetings = [];
+
+// for (let i = 0; i < names.length; i++) {
+// 	greetings[i] = "Hello, " + names[i] + "!";
+// 	console.log(greetings[i]);
+// }
+// objArray = [ { foo: 1, bar: 2}, { foo: 3, bar: 4}, { foo: 5, bar: 6} ];
+// function getFields(input, field) {
+//     var output = [];
+//     for (var i=0; i < input.length ; ++i)
+//         output.push(input[i][field]);
+//     return output;
+// }
+
+// var result = getFields(objArray, "foo"); // returns [ 1, 3, 5 ]
+// console.log(result)
+
+
+// let learnerSubmission =[{"learner_id" : 4,"assignment_id": number,"submission":{ "submitted_at": Date_string,"score": number}}]
+
+// learnerSubmission.forEach((row) => {
+// 	row.forEach((object) => {
+// 	  console.log(object.learner_id)
+
+// 	});
+// });
