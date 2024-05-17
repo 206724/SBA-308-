b2f3fd92ae3e93bde4c1d18f3b2e76bb65002690
Girma <93812274+206724@users.noreply.github.com>
2024-05-16 23:33:03 -0700

    my six commit

--- Script.mjs
+++ Script.mjs
@@ -188,13 +188,6 @@ const learner_id_125=(LearnerSubmissions.filter(
 
 
 
-// const learnerSubmission_learner_id=LearnerSubmissions.map(obj => 
-//     { if (obj.learner_id===125){
-//         return{...obj,assignment_id:1};
-//     }
-//     return obj;
-//   })
-// console.log(learnerSubmission_learner_id)
 
 
  
@@ -203,32 +196,11 @@ const learner_id_125=(LearnerSubmissions.filter(
  
  
  
- 
- 
- 
- 
-  // here, we would process this data to achieve the desired result.
-//     const result = [
-//       {
-//         id: 125,
-//         avg: 0.985, // (47 + 150) / (50 + 150)
-//         1: 0.94, // 47 / 50
-//         2: 1.0 // 150 / 150
-//       },
-//       {
-//         id: 132,
-//         avg: 0.82, // (39 + 125) / (50 + 150)
-//         1: 0.78, // 39 / 50
-//         2: 0.833 // late: (140 - 15) / 150
-//       }
-//     ];
+
   
-//     return result;
-//   }
+  // const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
-//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-//   console.log(result);
+  // console.log(result);
   
 
 
